9b5722cf4302b73c02852646505d6a2174153f26
Christoph Gysin <christoph.gysin@gmail.com>
2011-11-15 01:04:11 +0200

    basic alarm table and buttons, ClickListener example

--- schoolbell.py
+++ schoolbell.py
@@ -6,51 +6,98 @@ from pyjamas.ui.TextArea import TextArea
 from pyjamas.ui.Label import Label
 from pyjamas.ui.Button import Button
 from pyjamas.ui.Image import Image
+from pyjamas.ui.FlexTable import FlexTable
 from pyjamas.ui.VerticalPanel import VerticalPanel
 from pyjamas.ui.HorizontalPanel import HorizontalPanel
 from pyjamas.ui.ListBox import ListBox
+from pyjamas.ui.RowFormatter import RowFormatter
 from pyjamas.JSONService import JSONProxy
+
+class Alarms:
+    def __init__(self):
+        self.weekday_name = { 0: 'Mo', 1: 'Di', 2: 'Mi', 3: 'Do', 4: 'Fr', 5: 'Sa', 6: 'So' }
+        self.make_alarm_table()
+        self.fill_alarm_table()
+        self.status = Label('status')
+
+    def get_alarms(self):
+        return [
+                { 'time': '08:00:00', 'weekdays': [ 0, 1, 2, 3, 4, 5 ], 'duration': 3 },
+                { 'time': '08:55:00', 'weekdays': [ 0, 1, 2, 3, 4, 5 ], 'duration': 3 },
+                { 'time': '09:05:00', 'weekdays': [ 0, 1, 2, 3, 4, 5 ], 'duration': 3 },
+                { 'time': '10:00:00', 'weekdays': [ 0, 1, 2, 3, 4, 5 ], 'duration': 5 },
+                { 'time': '13:00:00', 'weekdays': [ 0, 1, 2, 3, 4 ], 'duration': 3 },
+                { 'time': '13:50:00', 'weekdays': [ 0, 1, 2, 3, 4 ], 'duration': 3 },
+                { 'time': '14:00:00', 'weekdays': [ 0, 1, 2, 3, 4 ], 'duration': 3 },
+        ]
+
+    def make_alarm_table(self):
+        self.table = FlexTable(StyleName='alarms')
+        self.table.setBorderWidth(1)
+        self.table.setText(0, 0, 'Time')
+        self.table.setText(0, 1, 'Days')
+        self.table.setText(0, 2, 'Duration')
+        for col in range(3):
+            self.table.getCellFormatter().setStyleName(0, col, 'tablecell header')
+
+        button = HorizontalPanel()
+        button.setSpacing(5)
+        image = Image('icons/plus.png')
+        image.addClickListener(self.plus_clicked)
+        button.add(image)
+        self.table.setWidget(1, 3, button)
+        for col in range(4):
+            self.table.getCellFormatter().setStyleName(1, col, 'tablecell noborder')
+
+    def plus_clicked(self):
+        self.status.setText('button clicked')
+
+    def fill_alarm_table(self):
+        for idx, alarm in enumerate(self.get_alarms()):
+            self.add_alarm(alarm['time'], alarm['weekdays'], alarm['duration'])
+
+    def add_alarm(self, time, weekdays=range(5), duration=3):
+        row = self.table.getRowCount()-1
+        self.table.insertRow(row)
+        self.table.setText(row, 0, time)
+        weekdays_str = []
+        for weekday in weekdays:
+            weekdays_str.append(self.weekday_name[weekday])
+
+        self.table.setText(row, 1, ', '.join(weekdays_str))
+        self.table.setText(row, 2, str(duration) + 's')
+
+        buttons = HorizontalPanel()
+        buttons.setSpacing(5)
+        buttons.add(Image('icons/e.png'))
+        buttons.add(Image('icons/x.png'))
+        self.table.setWidget(row, 3, buttons)
+
+        for col in range(3):
+            self.table.getCellFormatter().setStyleName(row, col, 'tablecell')
+        self.table.getCellFormatter().setStyleName(row, 3, 'tablecell noborder')
 
 class schoolbell:
 
+    def __init__(self):
+        pass
+
     def make_gui(self):
-        header = HorizontalPanel()
+        header = HorizontalPanel(StyleName='header')
         header.add(Image('icons/bell.png', StyleName='logo'))
         header.add(Label('Schoolbell', StyleName='title'))
 
-        self.status = Label()
-        self.text_area = TextArea()
-        self.text_area.setText("""foobar""")
-        self.text_area.setCharacterWidth(80)
-        self.text_area.setVisibleLines(8)
-
-        self.method_list = ListBox()
-        self.method_list.setName("hello")
-        self.method_list.setVisibleItemCount(1)
-        for method in self.methods:
-            self.method_list.addItem(method)
-        self.method_list.setSelectedIndex(1)
-
-        method_panel = HorizontalPanel()
-        method_panel.add(Label("Remote string method to call: "))
-        method_panel.add(self.method_list)
-        method_panel.setSpacing(8)
-
-        self.button_rpc = Button("Send to RPC", self)
-
-        buttons = HorizontalPanel()
-        buttons.add(self.button_rpc)
-        buttons.setSpacing(8)
+        alarms = Alarms()
+        alarm_panel = VerticalPanel()
+        #alarm_panel.add(Label('Alarms:'))
+        alarm_panel.add(alarms.table)
+        alarm_panel.add(alarms.status)
 
         panel = VerticalPanel()
         panel.add(header)
-        panel.add(self.text_area)
-        panel.add(method_panel)
-        panel.add(buttons)
-        panel.add(self.status)
+        panel.add(alarm_panel)
 
         RootPanel().add(panel)
-
 
     def onModuleLoad(self):
         self.methods = [ "Echo", "Reverse", "UPPERCASE", "lowercase", "nonexistent" ]
@@ -59,26 +106,10 @@ class schoolbell:
 
 
     def onClick(self, sender):
-        self.status.setText("Waiting for response...")
-        method = self.methods[self.method_list.getSelectedIndex()]
-        text = self.text_area.getText()
-
-        if sender == self.button_rpc:
-            remote = self.remote_rpc
-
-        if method == "Echo":
-            id = remote.echo(text, self)
-        elif method == "Reverse":
-            id = remote.reverse(text, self)
-        elif method == "UPPERCASE":
-            id = remote.uppercase(text, self)
-        elif method == "lowercase":
-            id = remote.lowercase(text, self)
-        elif method == "nonexistent":
-            id = remote.nonexistant(text, self)
+        pass
 
     def onRemoteResponse(self, response, request_info):
-        self.status.setText(response)
+        pass
 
     def onRemoteError(self, code, errobj, request_info):
         # onRemoteError gets the HTTP error code or 0 and
@@ -90,12 +121,10 @@ class schoolbell:
         #     }
         message = errobj['message']
         if code != 0:
-            self.status.setText("HTTP error %d: %s" % 
-                                (code, message))
+            print("HTTP error %d: %s" % (code, message))
         else:
             code = errobj['code']
-            self.status.setText("JSONRPC Error %s: %s" %
-                                (code, message))
+            print("JSONRPC Error %s: %s" % (code, message))
 
 
 class EchoServiceRPC(JSONProxy):
